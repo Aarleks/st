0de57bc88e5d680612cd095bddfeced7699e665e
Alex Norman <Aarleks@gmail.com>
2019-05-02 22:37:25 +1000

    Added Alt-Shift i/o for zoom in/out.

--- config.def.h
+++ config.def.h
@@ -190,8 +190,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
-	{ MODKEY,		XK_k,		kscrollup,      {.i =  1} },
+	{ MODKEY|ShiftMask,	XK_i,		zoom,           {.f = +1} },
+	{ MODKEY|ShiftMask,	XK_o,		zoom,           {.f = -1} },
 };
 
 /*
