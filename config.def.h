809f8f36e32e86f2220f1b9a9dd3830546283814
Alex Norman <Aarleks@gmail.com>
2019-05-03 10:31:58 +1000

    removed lowercase letters from zoom command

--- config.def.h
+++ config.def.h
@@ -190,6 +190,9 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ Mod1Mask,		XK_l,		externalpipe,	{ .v = openurlcmd }},
+	{ MODKEY,               XK_j,           kscrolldown,    {.i =  1} },
+	{ MODKEY,		XK_k,		kscrollup,      {.i =  1} },
 	{ MODKEY|ShiftMask,	XK_I,		zoom,           {.f = +1} },
 	{ MODKEY|ShiftMask,	XK_O,		zoom,           {.f = -1} },
 };
